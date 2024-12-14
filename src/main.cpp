90beab9861727e8c95e78c44c843180f3b75f3de
Aleksander Adamkowski <aleks.adamkowski@gmail.com>
2024-12-14 11:58:50 +0100

    Mqtt functionality added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "main.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  // Initialize Serial DEBUG
+  esp_log_level_set(LOG_TAG, ESP_LOG_VERBOSE);
+
+  // WiFi connection (you need to configure SSID and Password)
+  WiFi.begin("MexicoHotspot", "Nugget4lyfe");
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  ESP_LOGI("[WIFI]", "WiFi Connected");
+
+  // MQTT setup
+  mqtt.setServer(endpoint.c_str(), 1883);
+  ESP_LOGI("[MQTT]", "MQTT Configured");
+
+  mqtt.connect(token.c_str(),token.c_str(),"");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  mqtt.loop();  // Maintain MQTT connection
+  if (!mqtt.connected()) {
+      if (mqtt.connect(devicename.c_str(), token.c_str(), "")) {
+          ESP_LOGI("[MQTT]", "MQTT Connected");
+      } else {
+          ESP_LOGW("[MQTT]", "Failed to connect to MQTT");
+          delay(5000);  // Wait before retrying
+          return;
+      }
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  mqtt.publish(topic.c_str(), telemetry.c_str(), true);
+
+  delay(1000);  // Adjust delay as necessary
 }
