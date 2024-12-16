555d5ce12f1b59bd8992f7c601fdb98dae3fc581
rasmushoeg <rasmushoegmoeller@gmail.com>
2024-12-16 16:38:14 +0100

    Add BMP180 to sensor lib

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,51 @@ void setup() {
   esp_log_level_set(LOG_TAG, ESP_LOG_VERBOSE);
 
   // WiFi connection (you need to configure SSID and Password)
+<<<<<<< Updated upstream
   WiFi.begin("MexicoHotspot", "Nugget4lyfe");
+=======
+  WiFi.begin("#Telia-8D7FD0", "*A38Y@2c(zA=unK1");
+>>>>>>> Stashed changes
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
   }
   ESP_LOGI("[WIFI]", "WiFi Connected");
 
+  detect_Sensors();
+
   // MQTT setup
   mqtt.setServer(endpoint.c_str(), 1883);
   ESP_LOGI("[MQTT]", "MQTT Configured");
 
   mqtt.connect(token.c_str(),token.c_str(),"");
+
+  delay(1000);
+
 }
 
 void loop() {
+
+  //TODO: Read sensor func
+  read_BMP180();
+
   mqtt.loop();  // Maintain MQTT connection
   if (!mqtt.connected()) {
       if (mqtt.connect(devicename.c_str(), token.c_str(), "")) {
-          ESP_LOGI("[MQTT]", "MQTT Connected");
+          ESP_LOGI("MQTT", "MQTT Connected");
       } else {
-          ESP_LOGW("[MQTT]", "Failed to connect to MQTT");
+          ESP_LOGW("MQTT", "Failed to connect to MQTT");
           delay(5000);  // Wait before retrying
           return;
       }
   }
-
+  
+  PayloadPrepare();
+  String telemetry;
+  serializeJson( jsonTelemetry, telemetry );
+  ESP_LOGI("TELEMETRY", "%s", telemetry.c_str());
   mqtt.publish(topic.c_str(), telemetry.c_str(), true);
-  ESP_LOGI("[MQTT]", "Published");
+  ESP_LOGI("MQTT", "Published");
 
   delay(1000);  // Adjust delay as necessary
 }
